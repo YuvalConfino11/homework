7aa08e2a70be5c64990bde2a8b238d0e7c0be556
Omri-Lvy <omri.lvy@gmail.com>
2023-05-18 12:52:16 +0300

    fix movement

--- Assets/Scripts/Mobs/ExampleMob.cs
+++ Assets/Scripts/Mobs/ExampleMob.cs
@@ -17,15 +17,11 @@ namespace Mobs
         private MobStats m_mobStats;
         
         private GameObject m_PlayerGameObject;
-        private Rigidbody2D m_rigidbody2D;
-        private Collider2D m_collider2D;
 
         private void Awake()
         {
             StartCoroutine(playerHitCheck());
             m_mobStats = GetComponent<MobStats>();
-            m_rigidbody2D = GetComponent<Rigidbody2D>();
-            m_collider2D = GetComponent<Collider2D>();
         }
 
         private void Update()
