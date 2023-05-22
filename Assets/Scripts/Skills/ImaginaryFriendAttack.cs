4664a01f10b812e33f0936dbd63a44c1b66d98b8
Omri-Lvy <omri.lvy@gmail.com>
2023-05-22 11:31:09 +0300

    fix ImaginaryFriendAttack.cs location

---
+++ Assets/Scripts/Skills/ImaginaryFriendAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Skills
+{
+    [CreateAssetMenu (menuName = "Scriptable Objects/Imaginary Friend Attack" , fileName = "ImaginaryFriendAttack")]
+
+    public class ImaginaryFriendAttack : ScriptableObject
+    {
+
+        [SerializeField]
+        private short k_AvailabilityLevel = 2;
+        [SerializeField]
+        private float m_AttackRadius = 50;
+        [SerializeField]
+        private float m_AttackDamage = 30;
+        private readonly SkillsStats m_SkillsStats;
+
+
+
+        public ImaginaryFriendAttack()
+        {
+            m_SkillsStats = new SkillsStats(k_AvailabilityLevel);
+        }
+
+        public SkillsStats GetSkillsStats()
+        {
+            return m_SkillsStats;
+        }
+
+        public float GetAttackRadius()
+        {
+            return m_AttackRadius;
+        }
+
+        public float getAttackDamage()
+        {
+            return m_AttackDamage;
+        }
+        
+    }
+}
