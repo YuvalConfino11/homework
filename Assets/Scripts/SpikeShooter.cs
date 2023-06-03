e4aedae62105d0bb6c56eeaba1386603bd3e2a82
Omri-Lvy <omri.lvy@gmail.com>
2023-06-03 22:43:34 +0300

    Spikes demage and hit knockback WIP

--- Assets/Scripts/SpikeShooter.cs
+++ Assets/Scripts/SpikeShooter.cs
@@ -11,28 +11,15 @@ public class SpikeShooter : MonoBehaviour
     [SerializeField]
     private float m_TimeBetweenSpikes = 0.8f;
 
-    private float timer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float m_Timer;
+    
     void Update()
     {
-        timer += Time.deltaTime;
-        if(timer > m_TimeBetweenSpikes)
+        m_Timer += Time.deltaTime;
+        if(m_Timer > m_TimeBetweenSpikes)
         {
-            timer = 0;
-            Shoot();
+            m_Timer = 0;
         }
     }
-
-    private void Shoot()
-    {
-        Instantiate(m_Spike, m_SpikePos.position, Quaternion.identity);
-    }
-
-   
+    
 }
