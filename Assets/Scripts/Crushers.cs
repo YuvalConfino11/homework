5e87f76a866e9720fb5d1906d365f9c77010aab9
Yuval Confino <yuvalconfino11@gmail.com>
2023-06-07 17:40:02 +0300

    added reset walls and map extension

--- Assets/Scripts/Crushers.cs
+++ Assets/Scripts/Crushers.cs
@@ -8,35 +8,15 @@ public class Crushers : MonoBehaviour
     private float m_UpSpeed;
     [SerializeField]
     private float m_DownSpeed;
-    [SerializeField]
-    private Transform m_UpperPos;
-    [SerializeField]
-    private Transform m_LowerPos;
-    [SerializeField]
-    private ActivateCrushers m_Activate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    private bool m_Crush;
-   
     // Update is called once per frame
     void Update()
     {
- 
-            if (transform.position.y >= m_UpperPos.position.y && m_Activate.m_ActivateCrushers)
-            {
-                m_Crush = true;
-            }
-            if (transform.position.y <= m_LowerPos.position.y)
-            {
-                m_Crush = false;
-            }
-            if (m_Crush)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, m_LowerPos.position, m_DownSpeed * Time.deltaTime);
-            }
-            else
-            {
-                transform.position = Vector2.MoveTowards(transform.position, m_UpperPos.position, m_UpSpeed * Time.deltaTime);
-            }
         
     }
 }
